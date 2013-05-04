306b837a377d2f98b0757d7cb8cb98657b3fa666
Ryan Seys <rsmseys@gmail.com>
2013-05-04 15:14:57 -0400

    Show Unavailable when pic doesnt have gps info

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ var pics_folder = './pics/';
 function printExif(filename) {
   exif(pics_folder + filename, function(err, obj) {
     console.log('Filename: \t' + obj['file name']);
-    console.log('Altitude: \t' + obj['gps altitude']);
-    console.log('Date Time: \t' + obj['gps date time']);
-    console.log('Latitude: \t' + obj['gps latitude']);
-    console.log('Longitude: \t' + obj['gps longitude']);
-    console.log('Position: \t' + obj['gps position']);
-    console.log('Latitude Ref: \t' + obj['gps latitude ref']);
-    console.log('Longitude Ref: \t' + obj['gps longitude ref']);
-    console.log('Altitude Ref: \t' + obj['gps altitude ref']);
-    console.log('Time Stamp: \t' + obj['gps time stamp']);
-    console.log('Process Method: ' + obj['gps processing method']);
-    console.log('Date Stamp: \t' + obj['gps date stamp']);
+    console.log('Altitude: \t' + (obj['gps altitude'] || 'Unavailable'));
+    console.log('Date Time: \t' + (obj['gps date time'] || 'Unavailable'));
+    console.log('Latitude: \t' + (obj['gps latitude'] || 'Unavailable'));
+    console.log('Longitude: \t' + (obj['gps longitude'] || 'Unavailable'));
+    console.log('Position: \t' + (obj['gps position'] || 'Unavailable'));
+    console.log('Latitude Ref: \t' + (obj['gps latitude ref'] || 'Unavailable'));
+    console.log('Longitude Ref: \t' + (obj['gps longitude ref'] || 'Unavailable'));
+    console.log('Altitude Ref: \t' + (obj['gps altitude ref'] || 'Unavailable'));
+    console.log('Time Stamp: \t' + (obj['gps time stamp'] || 'Unavailable'));
+    console.log('Process Method: ' + (obj['gps processing method'] || 'Unavailable'));
+    console.log('Date Stamp: \t' + (obj['gps date stamp'] || 'Unavailable'));
   });
 }
 
