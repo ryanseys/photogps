2ad98c2d244fc195eec86dfe1341ed88dc14204e
Ryan Seys <rsmseys@gmail.com>
2013-05-05 22:28:30 -0400

    Add location and image separately to marker

--- public/js/photogps.js
+++ public/js/photogps.js
@@ -1,23 +1,16 @@
-var map;
-
-function addPoint(lat, lon, file_base64) {
-  var coords = new google.maps.LatLng(lat, lon);
-  var marker = new google.maps.Marker({
-    position: coords,
-    map: map,
-  });
-  addInfoWindow(marker, file_base64);
-}
+var map,
+dropbox = document.getElementById('map-canvas'),
+log = document.getElementById('log');
 
 function addInfoWindow(marker, message) {
   var info = message;
 
   var infoWindow = new google.maps.InfoWindow({
-      content: '<img src="' + message + '"/>'
+    content: '<img src="' + message + '"/>'
   });
 
   google.maps.event.addListener(marker, 'click', function () {
-      infoWindow.open(map, marker);
+    infoWindow.open(map, marker);
   });
 }
 
@@ -29,11 +22,6 @@ function initialize() {
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-var dropbox = document.getElementById('map-canvas');
-var log = document.getElementById('log');
 
 // Handle each file that was dropped (you can drop multiple at once)
 function drop(e) {
@@ -51,6 +39,15 @@ function no_bubble(e) {
   e.preventDefault();
 }
 
+// Update the progress bar
+function update_progress(e, label) {
+  if (e.lengthComputable) {
+    var progress = label.getElementsByTagName('progress')[0]
+    progress.setAttribute('value', e.loaded);
+    progress.setAttribute('max', e.total);
+  }
+}
+
 function upload_file(file) {
   // Make a progress bar
   var label = document.createElement('div');
@@ -63,6 +60,11 @@ function upload_file(file) {
       xhr = new XMLHttpRequest(),
       file_base64;
 
+  // var coords = new google.maps.LatLng(0, 0);
+  var marker = new google.maps.Marker({
+    map: map,
+  });
+
   reader.readAsDataURL(file);
   data.append('file', file);
 
@@ -73,23 +75,27 @@ function upload_file(file) {
     }, false);
   }
 
+  // image upload finished?
   xhr.onreadystatechange = function(e) {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         label.parentNode.removeChild(label);
         var coords = JSON.parse(xhr.responseText);
-        console.log('added point');
-        if(coords.lat && coords.lon) addPoint(coords.lat, coords.lon, file_base64);
+        if(coords.lat && coords.lon) {
+          marker.setPosition(new google.maps.LatLng(coords.lat, coords.lon));
+        }
       } else {
         console.log('An error occurred!');
       }
     }
   }
 
+  // image file loaded in filereader?
   reader.onloadend = function (event) {
     var img = new Image;
     img.src = event.target.result;
 
+    // image loaded in img?
     img.onload = function() {
       var maxWidth = 100,
         maxHeight = 100,
@@ -114,24 +120,19 @@ function upload_file(file) {
       canvas.height = imageHeight;
 
       var ctx = canvas.getContext("2d");
+      // redraw smaller
       ctx.drawImage(this, 0, 0, imageWidth, imageHeight);
-      file_base64 = canvas.toDataURL();
-      xhr.open('POST', '/upload', true);
-      xhr.send(data); //post!
+      addInfoWindow(marker, canvas.toDataURL());
     }
   };
-}
 
-// Update the progress bar
-function update_progress(e, label) {
-  if (e.lengthComputable) {
-    var progress = label.getElementsByTagName('progress')[0]
-    progress.setAttribute('value', e.loaded);
-    progress.setAttribute('max', e.total);
-  }
+  xhr.open('POST', '/upload', true);
+  xhr.send(data); //post!
 }
 
 dropbox.addEventListener("drop", drop, false);
 dropbox.addEventListener("dragleave", no_bubble, false);
 dropbox.addEventListener("dragexit", no_bubble, false)
 dropbox.addEventListener("dragover", no_bubble, false);
+
+google.maps.event.addDomListener(window, 'load', initialize);
