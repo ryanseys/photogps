7336241ce6d78f5c3f325c2b8e9a6793f987b073
Ryan Seys <rsmseys@gmail.com>
2013-05-06 22:09:41 -0400

    Simpler file loading dialog

--- public/js/photogps.js
+++ public/js/photogps.js
@@ -1,6 +1,7 @@
 var map,
 dropbox = document.getElementById('map-canvas'),
-log = document.getElementById('log');
+log = document.getElementById('log'),
+status = document.getElementById('status');
 
 function addInfoWindow(marker, message) {
   var info = message;
@@ -23,12 +24,16 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
+var num_files_currently_uploading = 0;
+var num_files_completed_this_round = 0;
+
 // Handle each file that was dropped (you can drop multiple at once)
 function drop(e) {
   no_bubble(e);
   var files = e.dataTransfer.files;
   for (var i = 0; i < files.length; i++) {
     upload_file(files[i]);
+    num_files_currently_uploading++;
   }
   return false;
 }
@@ -40,20 +45,22 @@ function no_bubble(e) {
 }
 
 // Update the progress bar
-function update_progress(e, label) {
+function update_progress(e) {
   if (e.lengthComputable) {
-    var progress = label.getElementsByTagName('progress')[0]
-    progress.setAttribute('value', e.loaded);
-    progress.setAttribute('max', e.total);
+    //status.innerHTML = "Loaded " + num_files_completed_this_round + "/" + num_files_currently_uploading;
   }
 }
 
-function upload_file(file) {
-  // Make a progress bar
-  var label = document.createElement('div');
-  label.innerHTML = '<progress></progress>' + file.name;
-  log.insertBefore(label, null);
+// function read_binary_data(file) {
+//   var r = new FileReader();
 
+//   r.readAsBinaryString(file);
+//   r.onloadend = function (event) {
+//     console.log(EXIF.readFromBinaryFile(file));
+//   }
+// }
+
+function upload_file(file) {
   // Build a form for the data
   var data = new FormData(),
       reader = new FileReader(),
@@ -64,20 +71,27 @@ function upload_file(file) {
       });
 
   reader.readAsDataURL(file);
+  //read_binary_data(file);
   data.append('file', file);
 
   // Periodically update progress bar
   if(xhr.upload) {
     xhr.upload.addEventListener('progress', function (e) {
-      update_progress(e, label);
+      update_progress(e);
     }, false);
   }
 
   // image upload finished?
   xhr.onreadystatechange = function(e) {
     if (xhr.readyState === 4) {
+      num_files_completed_this_round++;
+      if(num_files_currently_uploading == num_files_completed_this_round) {
+        num_files_currently_uploading = 0;
+        num_files_completed_this_round = 0;
+        status.innerHTML = "";
+      }
+      else status.innerHTML = "Loaded " + num_files_completed_this_round + "/" + num_files_currently_uploading;
       if (xhr.status === 200) {
-        label.parentNode.removeChild(label);
         var coords = JSON.parse(xhr.responseText);
         if(coords.lat && coords.lon) {
           marker.setPosition(new google.maps.LatLng(coords.lat, coords.lon));
